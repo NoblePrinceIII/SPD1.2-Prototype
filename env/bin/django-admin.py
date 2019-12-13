12bb61d02205e1ac780fd4b9bf178bbf327c1429
NoblePrinceii <farhan.begg@students.makeschool.com>
2019-12-13 12:30:35 -0800

    test

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nobleprince/dev/courses/Contractor2/blog/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ proposal.md
@@ -0,0 +1,3 @@
+A website where users will be able to post 
+their comments on news feed and only logged in users 
+will be able to see.
