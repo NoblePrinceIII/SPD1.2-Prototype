89fe822abaa9c1c61cd7524597565851ccffc65d
NoblePrinceii <farhan.begg@students.makeschool.com>
2019-12-13 14:22:34 -0800

    fix

--- blog/settings.py
+++ blog/settings.py
@@ -117,22 +117,22 @@ USE_TZ = True
 
 
 
-# Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/1.9/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
+Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-# # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-if os.getenv('IS_ON_HEROKU', False):
-    STATICFILE_DIRECTORY = 'static'
-else:
-    STATICFILE_DIRECTORY = 'static/assets'
-    STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, STATICFILE_DIRECTORY)
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# if os.getenv('IS_ON_HEROKU', False):
+#     STATICFILE_DIRECTORY = 'static'
+# else:
+#     STATICFILE_DIRECTORY = 'static/assets'
+#     STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, STATICFILE_DIRECTORY)
+# ]
 
 LOGIN_REDIRECT_URL = 'blog-home'
 
