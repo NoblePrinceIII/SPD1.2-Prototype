22f87c52a25d2b0e63adf46477cebe28767598be
NoblePrinceii <farhan.begg@students.makeschool.com>
2019-12-13 17:13:41 -0800

    complete

--- entries/tests.py
+++ entries/tests.py
@@ -1,31 +1,14 @@
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def setUp(self):
-        user = User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
-
-    def test_secure_page(self):
-        c = Client()
-        print c.login(username='temporary', password='temporary')
-        response = c.get('/users/', follow=True)
-        user = User.objects.get(username='temporary')
-        self.assertEqual(response.context['email'], 'temporary@gmail.com')
-
-    def test_true_is_true(self):
-        """Tests that True equals True"""
-        self.assertEqual(True, True)
-
 class PageDetailView_Test(TestCase):
     """Tests that the Detail Page works."""
     def test_detail_page(self):
-        #Create a user for this test 
-        user = User.objects.create()
-
-        #Create a page to the database 
-        Page.objects.create(title="My Test Page", content="test", author=user)
-
 
         #Make a GET request, and test route returns 200
         reponse = self.client.get("/my-test-page/")
 
         self.assertEqual(reponse.status_code, 200)
+
+    def test_true_is_true(self):
+        """Tests that True equals True"""
+        self.assertEqual(True, True)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SimpleTest(TestCase):
+    def setUp(self):
+        user = User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
+
+    def test_secure_page(self):
+        c = Client()
+        print c.login(username='temporary', password='temporary')
+        response = c.get('/users/register', follow=True)
+        user = User.objects.get(username='temporary')
+        self.assertEqual(response.context['email'], 'temporary@gmail.com')
